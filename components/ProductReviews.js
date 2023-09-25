65bc35c2ffc74933e6a0ff831e9f8734f6820a16
vladihka <vladihka58@gmail.com>
2023-09-25 13:35:37 +0200

    Fix keys

--- components/ProductReviews.js
+++ components/ProductReviews.js
@@ -120,7 +120,7 @@ export default function ProductReviews({product}){
                             <p>No reviews :(</p>
                         )}
                         {reviews.length > 0 && reviews.map(review => (
-                            <ReviewWrapper>
+                            <ReviewWrapper key={review._id}>
                                 <ReviewHeader>
                                     <StarsRating size={'sm'} disabled={true} defaultHowMany={review.stars}></StarsRating>
                                     <time>{(new Date(review.createdAt)).toLocaleString('sv-SE')}</time>
